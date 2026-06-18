7e4d77645e23aee45e7ed37b2779e1fef03119d7
Naaz-0001 <sishehna@sheridancollege.ca>
2026-06-18 15:21:56 -0400

    sample objects created

---
+++ midtermExam2Shehnazpreet/functions.swift
@@ -0,0 +1,8 @@
+//
+//  functions.swift
+//  midtermExam2Shehnazpreet
+//
+//  Created by Shehnazpreet Singh on 2026-06-18.
+//
+
+import Foundation
---
+++ midtermExam2Shehnazpreet/validations.swift
@@ -0,0 +1,8 @@
+//
+//  validations.swift
+//  midtermExam2Shehnazpreet
+//
+//  Created by Shehnazpreet Singh on 2026-06-18.
+//
+
+import Foundation
