f02289c7ff1e469edb8718b036d0f93ce774e0d4
Naaz-0001 <sishehna@sheridancollege.ca>
2026-06-18 15:22:43 -0400

    sample objects created for all

--- midtermExam2Shehnazpreet/main.swift
+++ midtermExam2Shehnazpreet/main.swift
@@ -7,5 +7,63 @@
 
 import Foundation
 
+var book1: Book = Book( itemID: "abc1", title: "The Great Gatsby", isBorrowed: true, author: "naaz", numberOfPages: 100)
+var book2: Book = Book( itemID: "abc2", title: "The Great Gatsby", isBorrowed: true, author: "naaz", numberOfPages: 100)
 
+var mag1: Magazine = Magazine(itemID: "abc3", title: "The Great Gatsby", isBorrowed: true, issueNumber: "qweeww", publicationMonth: "Jan")
+var mag2: Magazine = Magazine(itemID: "abc4", title: "The Great Gatsby", isBorrowed: true, issueNumber: "qweeww", publicationMonth: "Jan")
 
+var libraryItems: [String:LibraryItem] = [
+    "abc1" : book1,
+    "abc2" : book2,
+    "abc3" : mag1,
+    "abc4" : mag2
+]
+
+//for book in libraryItems.values {
+//    print("\(book.itemID) \(book.title) \(book.isBorrowed)")
+//}
+
+func menu() {
+    print()
+    print("Library Management System")
+    print()
+    print("1. Add a Book")
+    print("2. Add a Magazine")
+    print("3. Display all items")
+    print("4. Search Item")
+    print("5. Borrow Item")
+    print("6. Return Item")
+    print("7. Delete Item")
+    print("8. Exit")
+    print()
+    print("Enter your choice: ")
+    
+}
+while true {
+    menu()
+    let userInput : String = readLine()!
+    switch userInput {
+        
+        
+    case "1": addFtEmp()
+        
+    case "2": addPtEmp()
+        
+    case "3": displayEmp()
+        
+    case "4": removeEmp()
+        
+    case "5": searchEmp()
+        
+    case "6":
+        print("\n Exiting...")
+        break
+        
+    default : print("Invalid option, try again")
+    }
+    if userInput == "6" {
+        break;
+    }
+    
+}
